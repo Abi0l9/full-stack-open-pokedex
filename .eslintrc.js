7658950cc0572737a20a96df71523d5b42fbd0c0
Abi0l9 <cryptog3ms@gmail.com>
2024-04-16 10:58:57 +0100

    11.6 Fix the code - fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     indent: ["error", 2],
     "linebreak-style": ["error", "windows"],
     quotes: ["error", "double"],
-    semi: ["error", true],
+    semi: ["error", "always"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
