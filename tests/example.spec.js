cf7ba32f8f9cbd31b9c709b9af0d5b3a75716ce4
Abi0l9 <oyedejimonsur@gmail.com>
2024-08-14 15:13:17 +0100

    11.9 Simple end-to-end tests

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -24,8 +24,15 @@ const { test, describe, expect } = require("@playwright/test");
 describe("Pokedex", () => {
   test("front page can be opened", async ({ page }) => {
     const pokemonName = "ivysaur";
-    await page.goto("http://localhost:8080");
+    await page.goto("/");
     await expect(page.getByText(pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1))).toBeVisible();
     await expect(page.getByText("Pokémon and Pokémon character names are trademarks of Nintendo.")).toBeVisible();
+  });
+
+  test("pokemon page can be navigated to", async ({ page }) => {
+    const pokemonName = "ivysaur";
+    await page.goto("/pokemon/ivysaur");
+    await expect(page.getByText(pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1))).toBeVisible();
+    await expect(page.getByText("chlorophyll")).toBeVisible();
   });
 });
